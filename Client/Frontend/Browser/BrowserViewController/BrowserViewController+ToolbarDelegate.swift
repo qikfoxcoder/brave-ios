f2a9f19ba58d0316059e6c5e9cda51914d4c2cfa
Michał Buczek <michal.buczek@protonmail.com>
2022-02-15 20:29:19 +0100

    wip. moved swift ui code, need blurred button on ios 14

--- Client/Frontend/Browser/BrowserViewController/BrowserViewController+ToolbarDelegate.swift
+++ Client/Frontend/Browser/BrowserViewController/BrowserViewController+ToolbarDelegate.swift
@@ -258,7 +258,11 @@ extension BrowserViewController: TopToolbarDelegate {
     }
     
     func topToolbarDidTapBraveRewardsButton(_ topToolbar: TopToolbarView) {
-        showBraveRewardsPanel()
+        print("bxx aa")
+        
+        
+        
+        //showBraveRewardsPanel()
     }
     
     func topToolbarDidLongPressBraveRewardsButton(_ topToolbar: TopToolbarView) {
